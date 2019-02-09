a48489bb6a790ae9844697a803a4b294cd0b8708
Robbin Johansson <mail@robbin.works>
2019-02-09 14:44:05 +0100

    wip

--- source/_layouts/app.blade.php
+++ source/_layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title>robbin.works</title>
-        <meta name="description" content="I'm Robbin, a web wizard from Sweden who enjoys building great web apps.">
+        <meta name="description" content="I'm Robbin, I enjoy building great web apps.">
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     </head>
     <body class="font-sans font-normal leading-normal tracking-normal text-base md:text-lg text-gray-darker flex flex-wrap">
