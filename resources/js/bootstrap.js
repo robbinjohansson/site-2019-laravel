8c3870b73e0584ab82cf6b04cacb143a87f69f30
Robbin Johansson <mail@robbin.works>
2019-04-07 20:17:15 +0200

    prismjs

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,3 +1,7 @@
+
+const Prism = require('./extras/prism');
+Prism.highlightAll();
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
