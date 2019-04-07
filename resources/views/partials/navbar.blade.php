ce57a8aa1210a9091dae432664d9a592d6ebb21a
Robbin Johansson <mail@robbin.works>
2019-04-07 14:23:03 +0200

    navbar style fix

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,4 +1,4 @@
-<header class="bg-white text-xs p-2 mb-12">
+<header class="bg-white text-xs py-2 mb-12">
     <div class="container">
         <div class="md:flex md:flex-wrap -mx-4 md:justify-center">
             <div class="w-full md:w-1/2 px-4 flex flex-wrap items-center justify-between">
@@ -6,9 +6,9 @@
                     <a href="/">Robbin Johansson</a>
                 </div>
                 <nav class="flex-1 text-right">
-                    <a href="{{ route('about') }}" class="ml-3">About</a>
-                    <a href="{{ route('posts.archive') }}" class="ml-3">Archive</a>
-                    <a href="https://github.com/robbinworks" target="_BLANK" rel="noopener" class="ml-3">Github</a>
+                    <a href="{{ route('about') }}" class="ml-2 md:ml-3">About</a>
+                    <a href="{{ route('posts.archive') }}" class="ml-2 md:ml-3">Archive</a>
+                    <a href="https://github.com/robbinworks" target="_BLANK" rel="noopener" class="ml-2 md:ml-3">Github</a>
                 </nav>
             </div>
         </div>
