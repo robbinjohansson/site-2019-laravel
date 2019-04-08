b4ba22e04d781fd93f8c8597f09e837791f3fcd0
Robbin Johansson <mail@robbin.works>
2019-04-08 08:30:44 +0200

    wip

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -7,7 +7,7 @@
             <h1 class="mb-3">About</h1>
             <div class="markdown">
                 <p>I'm a web wizard from Sweden who enjoys developing great web applications, explore new technologies, contribute to open source projects and to spend time with my family.</p>
-                <p>PHP, Laravel, Javascript, Vuejs, Tailwindcss, Webpack, Rollupjs, CSS, SASS, LESS, HTML are all included in my daily workflow.</p>
+                <p>Occasionally I will blog about web related things, mostly as a way to keep notes for my future self. Hopefully these notes can be of value to other fellow devs as well.</p>
                 <img src="/images/about.jpg" alt="about">
                 <p>☝🏻Photo taken by <a href="http://github.com/johanlinander" target="_BLANK" rel="noopener">@johanlinander.</a> It's a few years old but it was the best I could find, it was either that or a random image of a cat.</p>
                 <p>Full source code of this website is open source and available over at <a href="https://github.com/robbinworks/site" target="_BLANK" rel="noopener">GitHub.</a></p>
