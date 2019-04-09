5a6ceeba4e4e429a60649ae6020a7e25d73f87c3
Robbin Johansson <mail@robbin.works>
2019-04-09 22:25:00 +0200

    wip

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
-# My personal website
+# Personal website
 
 [https://robbin.works/](https://robbin.works/)
 
 ## Tech stack
  
-- [Laravel](https://laravel.com) 5.8
+- [Laravel](https://laravel.com/) 5.8
 - [Vuejs](https://vuejs.org/) 2.6
 - [Tailwindcss](https://tailwindcss.com/docs/what-is-tailwind/) 1.0
-- Content is stored & retrieved in markdown using [`spatie/sheets`](https://github.com/spatie/sheets)
-- Static bundle of the site is exported using [`spatie/laravel-export`](https://github.com/spatie/laravel-export)
+- Assets compiled with [`JeffreyWay/laravel-mix`](https://github.com/JeffreyWay/laravel-mix/)
+- Unused CSS is purged using [`spatie/laravel-mix-purgecss`](https://github.com/spatie/laravel-mix-purgecss/)
+- Content is stored in markdown with front matter using [`spatie/sheets`](https://github.com/spatie/sheets/)
+- Static bundle of the site is exported using [`spatie/laravel-export`](https://github.com/spatie/laravel-export/)
 - The live site is hosted on [Netlify](https://www.netlify.com/)
 
 ## License
 
-- The web application falls under the [MIT License](https://choosealicense.com/licenses/mit/)
-- The content and design are under [exclusive copyright](https://choosealicense.com/no-license/)
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
 
 For questions or information drop me an email me at mail@robbin.works.
--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -6,15 +6,15 @@
         <div class="w-full md:w-3/4 lg:w-1/2 px-4">
             <h1 class="mb-3">About</h1>
             <div class="markdown">
-                <p>I'm a web wizard from Sweden who enjoys developing great web applications, explore new technologies, contribute to open source projects and to spend time with my family.</p>
-                <p>Occasionally I will blog about web related things, mostly as a way to keep notes for my future self. Hopefully these notes can be of value to other fellow devs as well.</p>
+                <p>I'm a web developer from Sweden who enjoys developing great web applications, explore new technologies, contribute to open source projects and to spend time with my family.</p>
+                <p>Occasionally I will post neat little tips and tricks related to web development, mostly as a way to keep notes for my future self. Hopefully these notes can be of value to other fellow devs as well.</p>
                 <img src="/images/about.jpg" alt="about">
                 <p>☝🏻Photo taken by <a href="http://github.com/johanlinander" target="_BLANK" rel="noopener">@johanlinander</a>. It's a few years old but it was the best I could find, it was either that or a random image of a cat.</p>
                 <p>Full source code of this website is open source and available over at <a href="https://github.com/robbinworks/site" target="_BLANK" rel="noopener">GitHub</a>.</p>
             </div>
             <h2 class="mt-10 mb-8 pt-10 border-t">Recent projects</h2>
             <div class="mb-10">
-                <h3><a href="#">v-siema</a></h3>
+                <h3><a href="https://github.com/robbinworks/v-siema" target="_BLANK" rel="noopener">v-siema</a></h3>
                 <div class="mb-2">
                     <div class="text-xs font-bold text-gray-500">
                         #vue #vue-component #carousel
@@ -26,7 +26,7 @@
                 <a href="https://github.com/robbinworks/v-siema" target="_BLANK" rel="noopener">View on GitHub &rarr;</a>
             </div>
             <div class="mb-10">
-                <h3><a href="#">vuebbble</a></h3>
+                <h3><a href="https://github.com/robbinworks/vuebbble" target="_BLANK" rel="noopener">vuebbble</a></h3>
                 <div class="mb-2">
                     <div class="text-xs font-bold text-gray-500">
                         #vue #vue-component #dribbble-api
@@ -38,7 +38,7 @@
                 <a href="https://github.com/robbinworks/vuebbble" target="_BLANK" rel="noopener">View on GitHub &rarr;</a>
             </div>
             <div class="mb-10">
-                <h3><a href="#">vue-scroll-indicator</a></h3>
+                <h3><a href="https://github.com/robbinworks/vue-scroll-indicator" target="_BLANK" rel="noopener">vue-scroll-indicator</a></h3>
                 <div class="mb-2">
                     <div class="text-xs font-bold text-gray-500">
                         #vue #vue-component #js-scroll-indicator
@@ -50,7 +50,7 @@
                 <a href="https://github.com/robbinworks/vue-scroll-indicator" target="_BLANK" rel="noopener">View on GitHub &rarr;</a>
             </div>
             <div>
-                <h3><a href="#">haywire-grav</a></h3>
+                <h3><a href="https://github.com/robbinworks/haywire-grav" target="_BLANK" rel="noopener">haywire-grav</a></h3>
                 <div class="mb-2">
                     <div class="text-xs font-bold text-gray-500">
                         #grav-cms #grav-starter-theme
