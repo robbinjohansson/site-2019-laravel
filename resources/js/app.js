e44bab5aa52e598a256648d90064def8fa437ac0
Robbin Johansson <mail@robbin.works>
2019-04-12 23:13:35 +0200

    remove vue for now

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,3 @@
 
 const Prism = require('./extras/prism');
 Prism.highlightAll();
-
-window.Vue = require('vue');
-
-import ExampleComponent from './components/ExampleComponent.vue';
-Vue.component('example-component', ExampleComponent);
-
-const app = new Vue({
-    el: '#app',
-});
